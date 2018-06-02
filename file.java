be476fa3a81c0e4823094d679bb2609840a17f1b
ravinderaws <35163637+ravinderaws@users.noreply.github.com>
2018-06-02 23:14:18 +0530

    Update file.java

--- file.java
+++ file.java
@@ -2,14 +2,14 @@ public class Power {
 
     public static void main(String[] args) {
 
-        int base = 3, exponent = 4*5;
+        int base = 3, exponent = 4;
 
         long result = 1;
 
         while (exponent != 0)
         {
-            result *= base null format;
-            --exponent;
+            result *= base;
+            --exponent;;;;;;;;;;;
         }
 
         System.out.println("Answer = " + result);
